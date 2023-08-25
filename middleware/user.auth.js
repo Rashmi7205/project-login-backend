df19b3e1ce93863fe76202ab60c75d7ebe54d8e3
Rashmi7205 <rashmiranjanbehera8260@gmail.com>
2023-08-25 19:11:08 +0530

    changes

--- middleware/user.auth.js
+++ middleware/user.auth.js
@@ -7,7 +7,7 @@ const isLoggedIn = async (req,res,next)=>{
     try {
         const {token} = req.cookies;
         if(!token){
-            return next(new AppError('Unauthenticated User',400));
+            return next(new AppError('Unauthrized  User',400));
         }
         const userDetails = await Jwt.verify(token,process.env.JWT_SECRET);
        
